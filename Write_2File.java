f93e6c757e27a48287e87149711a903390ac695f
HiveMind <Zerodavila11@gmail.com>
2016-05-23 18:39:12 +0300

    Create Write_2File.java

---
+++ Write_2File.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Create_Write2File {
+
+	public static void main(String[] args) {
+		
+		try{
+		File file = new File("fileName.txt");
+		
+		if(file.exists()){
+				file.createNewFile();
+				
+			}
+			
+			PrintWriter pw = new PrintWriter(file);
+			pw.println("this is my file content");
+			pw.println(100000);
+			pw.close();
+			System.out.println("Done");
+			
+			} catch (IOException e) {
+				e.printStackTrace();
+		}
+	}
+}
