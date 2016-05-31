a707b547728aabf6b14f7a55a78c5bc9e9a98fd9
HiveMind <Zerodavila11@gmail.com>
2016-05-31 14:57:35 +0200

    Add files via upload

---
+++ JavaWhiteTile/Renderer.java
@@ -0,0 +1,24 @@
+package dontTouchTheWhiteTile;
+/*
+ * author Jaryt Bustard
+ */
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Renderer extends JPanel
+{
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+
+		if (DontTouchTheWhiteTile.dttwt != null)
+		{
+			DontTouchTheWhiteTile.dttwt.render(g);
+		}
+	}
+	
+}
