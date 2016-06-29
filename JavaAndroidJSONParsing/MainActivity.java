6a41effa5715a8067e43754c98f47f3e125ff60b
HiveMind <Zerodavila11@gmail.com>
2016-06-29 16:52:49 -0400

    Create MainActivity.java

---
+++ JavaAndroidJSONParsing/MainActivity.java
@@ -0,0 +1,256 @@
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.hivemind.root.jsonparsingtutorial.com.root.hivemind.jsonparsingtutorial.models.MovieModel;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lvMovies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create default options which will be used for every
+        //  displayImage(...) call if no options will be passed to this method
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+        .build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+        .defaultDisplayImageOptions(defaultOptions)
+        .build();
+
+        ImageLoader.getInstance().init(config); // Do it on Application start
+
+        lvMovies = (ListView)findViewById(R.id.lvMovies);
+
+                //new JSONTask().execute("http://jsonparsing.parseapp.com/jsonData/moviesData.txt");
+                //new JSONTask().execute("http://jsonparsing.parseapp.com/jsonData/moviesDemoList.txt");
+                //new JSONTask().execute("http://jsonparsing.parseapp.com/jsonData/moviesDemoItem.txt");
+    }
+
+    public class JSONTask extends AsyncTask<String, String, List<MovieModel>>{
+
+        @Override
+        protected List<MovieModel> doInBackground(String... params) {
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                String finalJson = buffer.toString();
+
+                JSONObject parentObject = new JSONObject(finalJson);
+                JSONArray parentArray = parentObject.getJSONArray("movies");
+
+                List<MovieModel> movieModelList = new ArrayList<>();
+
+                for(int i=0; i<parentArray.length(); i++) {
+                    JSONObject finalObject = parentArray.getJSONObject(i);
+                    MovieModel movieModel = new MovieModel();
+                    movieModel.setMovie(finalObject.getString("movie"));
+                    movieModel.setYear(finalObject.getInt("year"));
+                    movieModel.setRating((float) finalObject.getDouble("rating"));
+                    movieModel.setDirector(finalObject.getString("director"));
+                    movieModel.setDuration(finalObject.getString("duration"));
+                    movieModel.setTagline(finalObject.getString("tagline"));
+                    movieModel.setImage(finalObject.getString("image"));
+                    movieModel.setStory(finalObject.getString("story"));
+
+                    List<MovieModel.Cast> castList = new ArrayList<>();
+                    for(int j=0; i<finalObject.getJSONArray("cast").length(); j++) {
+                        MovieModel.Cast cast = new MovieModel.Cast();
+                        cast.setName(finalObject.getJSONArray("cast").getJSONObject(j).getString("name"));
+                        castList.add(cast);
+                    }
+                    movieModel.setCastList(castList);
+                    //adding the final object in the list
+                    movieModelList.add(movieModel);
+
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(List<MovieModel> result) {
+            super.onPostExecute(result);
+
+            MovieAdaptor adapter = new MovieAdaptor(getApplicationContext(), R.layout.row, result);
+            lvMovies.setAdapter(adapter);
+            // TODO need to set data to the list
+        }
+    }
+
+    public class MovieAdaptor extends ArrayAdapter {
+
+        private List<MovieModel> movieModelList;
+        private int resource;
+        private LayoutInflater inflater;
+        public MovieAdaptor(Context context, int resource, List<MovieModel> objects) {
+            super(context, resource, objects);
+            movieModelList = objects;
+            this.resource = resource;
+            inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+
+            if(convertView == null) {
+                holder = new ViewHolder();
+                convertView = inflater.inflate(resource, null);
+                holder.ivMovieIcon = (ImageView)convertView.findViewById(R.id.ivIcon);
+                holder.tvMovie = (TextView)convertView.findViewById(R.id.tvMovie);
+                holder.tvTagline = (TextView)convertView.findViewById(R.id.tvTagline);
+                holder.tvYear = (TextView)convertView.findViewById(R.id.tvYear);
+                holder.tvDuration = (TextView)convertView.findViewById(R.id.tvDuration);
+                holder.tvDirector = (TextView)convertView.findViewById(R.id.tvDirector);
+                holder.rtMovieRating = (RatingBar) convertView.findViewById(R.id.rtMovie);
+                holder.tvCast = (TextView)convertView.findViewById(R.id.tvCast);
+                holder.tvStory = (TextView)convertView.findViewById(R.id.tvStory);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            final ProgressBar progressBar = (ProgressBar)convertView.findViewById(R.id.progressBar);
+
+            //Display the image
+            ImageLoader.getInstance().displayImage(movieModelList.get(position).getImage(), holder.ivMovieIcon, new ImageLoadingListener() {
+                @Override
+                public void onLoadingStarted(String imageUri, View view) {
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                    progressBar.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    progressBar.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onLoadingCancelled(String imageUri, View view) {
+                    progressBar.setVisibility(View.GONE);
+                }
+            });
+
+            holder.tvMovie.setText(movieModelList.get(position).getMovie());
+            holder.tvTagline.setText(movieModelList.get(position).getTagline());
+            holder.tvYear.setText(String.format("Year : %d", movieModelList.get(position).getYear()));
+            holder.tvDuration.setText(String.format("Duration : %s", movieModelList.get(position).getDuration()));
+            holder.tvDirector.setText(String.format("Director : %s", movieModelList.get(position).getDirector()));
+            holder.rtMovieRating.setRating(movieModelList.get(position).getRating()/2);
+
+            StringBuffer stringBuffer = new StringBuffer();
+            for(MovieModel.Cast cast : movieModelList.get(position).getCastList()) {
+                stringBuffer.append(cast.getName() + ", ");
+            }
+
+            holder.tvCast.setText(stringBuffer);
+            holder.tvStory.setText(movieModelList.get(position).getStory());
+            return convertView;
+        }
+
+        class ViewHolder {
+
+            private ImageView ivMovieIcon;
+            private TextView tvMovie;
+            private TextView tvTagline;
+            private TextView tvYear;
+            private TextView tvDuration;
+            private TextView tvDirector;
+            private RatingBar rtMovieRating;
+            private TextView tvCast;
+            private TextView tvStory;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            new JSONTask().execute("http://jsonparsing.parseapp.com/jsonData/moviesData.txt");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
