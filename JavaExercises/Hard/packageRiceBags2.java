2f039e54f825d99bc24ede3cdfef21d402342698
HiveMind <Zerodavila11@gmail.com>
2016-08-01 18:00:57 +0200

    Add files via upload

---
+++ JavaExercises/Hard/packageRiceBags2.java
@@ -0,0 +1,12 @@
+public Integer getNumberSmallBags(Integer big, Integer small, Integer goal) {
+    int result = -1;
+    if (big * 5 >= goal) {
+        if (small >= goal % 5)
+            result = goal % 5;
+    } else {
+        if (small >= goal - big * 5) {
+            result = goal - big * 5;
+        }
+    }
+    return result;
+}
