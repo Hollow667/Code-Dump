18f715601c1d8f0f4e176b8b53e4393a398d6583
HiveMind <Zerodavila11@gmail.com>
2016-08-14 18:01:38 +0200

    Add files via upload

---
+++ guitar_tabgen.py
@@ -0,0 +1,36 @@
+
+# Generator de tabulaturi
+
+with open("TabulaturaNoua.txt", 'w') as f:
+
+    f.write ("\n\n")
+
+    f.write (70 * "-")
+    f.write ("\n")
+    f.write (70 * "-")
+    f.write ("\n")
+    f.write (70 * "-")
+    f.write ("\n")
+    f.write (70 * "-")
+    f.write ("\n")
+    f.write (70 * "-")
+    f.write ("\n")
+    f.write (70 * "-")
+
+    f.write ("\n\n")
+
+    f.write (70 * "-")
+    f.write ("\n")
+    f.write (70 * "-")
+    f.write ("\n")
+    f.write (70 * "-")
+    f.write ("\n")
+    f.write (70 * "-")
+    f.write ("\n")
+    f.write (70 * "-")
+    f.write ("\n")
+    f.write (70 * "-")
+
+    f.write ("\n\n")
+    f.close()
+
