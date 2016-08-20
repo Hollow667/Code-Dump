4b54148a5b46d781b21e25ac99963af614120cb0
HiveMind <Zerodavila11@gmail.com>
2016-08-20 03:02:39 +0300

    Update dbDeleteOperation.py

--- PythonSqlite3/dbDeleteOperation.py
+++ PythonSqlite3/dbDeleteOperation.py
@@ -1,20 +1,19 @@
-#!/usr/bin/python
 
 import sqlite3
 
 conn = sqlite3.connect('test.db')
-print "Opened database successfully";
+print "Baza de date a fost deschisa.";
 
-conn.execute("DELETE from COMPANY where ID=2;")
+conn.execute("DELETE from EXEMPLU where ID=2;")
 conn.commit
-print "Total number of rows deleted :", conn.total_changes
+print "Numarul de randuri sterse :", conn.total_changes
 
-cursor = conn.execute("SELECT id, name, address, salary  from COMPANY")
+cursor = conn.execute("SELECT id, nume, adresa, salar  from EXEMPLU")
 for row in cursor:
    print "ID = ", row[0]
-   print "NAME = ", row[1]
-   print "ADDRESS = ", row[2]
-   print "SALARY = ", row[3], "\n"
+   print "NUME = ", row[1]
+   print "ADRESA = ", row[2]
+   print "SALAR = ", row[3], "\n"
 
-print "Operation done successfully";
+print "Proces incheiat.";
 conn.close()
