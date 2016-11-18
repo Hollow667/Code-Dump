bac4127c2a202ad6385a779242c0acb6e72a46ae
HiveMind <Zerodavila11@gmail.com>
2016-11-18 04:14:29 +0200

    Update slowprinter.sh

--- BashDump/slowprinter.sh
+++ BashDump/slowprinter.sh
@@ -1,10 +1,35 @@
 #!/usr/bin/bash
+
 filename="$1"
 counter=$((1))
+pre=$((1))
+
+read -p "Start lines> " START
+read -p "Sleep time> " SLEEP
+read -p "Pre seconds> " PRE
+echo
+
+while [[ $pre -le PRE ]]
+do
+    printf "...$pre"
+    pre=$((pre+1))
+sleep 1
+done
+
+echo
+echo
+
 while read -r line
 do
-    name="$line"
-    echo "$counter - $name"
-    counter=$((counter+1))
-    sleep 1
+    if [[ $counter -lt $START ]]
+    then
+	name="$line"
+	echo "$counter - $name"
+	counter=$((counter+1))
+    else
+    	name="$line"
+    	echo "$counter - $name"
+    	counter=$((counter+1))
+    	sleep $SLEEP
+    fi
 done < "$filename"
