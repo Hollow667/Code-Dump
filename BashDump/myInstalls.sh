ae85880cfd878a9412a02df1baf79e655a502aca
HiveMind <Zerodavila11@gmail.com>
2016-12-15 05:07:27 +0200

    Add files via upload

---
+++ BashDump/myInstalls.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+sudo apt-get install tmux -y
+echo "[OK] Tmux"
+sudo apt-get install elinks -y
+echo "[OK] Elinks"
+sudo apt-get install vim -y 
+echo "[OK] Vim"
+sudo apt-get install espeak -y
+echo "[OK] Espeak"
+sudo apt-get install jp2a -y 
+echo "[OK] Jp2a"
+sudo apt-get install cmus -y
+echo "[OK] Cmus"
+sudo apt-get install audacity -y
+echo "[OK] Audacity"
+sudo apt-get install gimp -y
+echo "[OK] Gimp"
+sudo apt-get install openshot -y
+echo "[OK] Openshot"
+sudo apt-get install jd-gui -y
+echo "[OK] Jd-gui"
+sudo apt-get install officelibre -y
+echo "[OK] Officelibre"
+sudo apt-get install deluge -y
+echo "[OK] Deluge"
+sudo apt-get install qmmp -y
+echo "[OK] Qmmp"
+sudo apt-get install pdftotext -y
+echo "[OK] Pdftotext"
+sudo apt-get install mplayer -y
+echo "[OK] Mplayer"
+sudo apt-get install youtube-dl -y
+echo "[OK] Youtube-dl"
+sudo apt-get install figlet -y
+echo "[OK] Figlet"
+sudo apt-get install calcurse -y 
+echo "[OK] Calcurse"
+sudo apt-get install inotify-tools -y
+echo "[OK] Inotify-tools"
+sudo apt-get install iptraf -y
+echo "[OK] Iptraf"
+sudo apt-get install nload -y
+echo "[OK] Nload"
+sudo apt-get install iftop -y
+echo "[OK] Iftop"
+sudo apt-get install tcptrack -y
+echo "[OK] Tcptrack"
+sudo apt-get install mailutils -y
+echo "[OK] Mailutils"
+sudo apt-get install clamav -y
+echo "[OK] Clamav"
+sudo apt-get install gufw -y
+echo "[OK] Gufw"
+echo "[*] Done!"
